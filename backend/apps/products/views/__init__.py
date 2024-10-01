a34c2abfa59809250abb7f783b4348a644e64ac0
nstan <nstanislass@gmail.com>
2024-10-02 00:52:04 +0300

    add TMPProductCreateAPIView

--- backend/apps/products/views/__init__.py
+++ backend/apps/products/views/__init__.py
@@ -1,2 +1,3 @@
 from apps.products.views.category import CategoryAPIViewSet
 from apps.products.views.product import ProductAPIViewSet
+from apps.products.views.tmp_product import TMPProductCreateAPIView
