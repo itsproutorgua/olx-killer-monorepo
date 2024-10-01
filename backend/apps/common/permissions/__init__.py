c2c265445100c49d743e320d1ab57008e68506b3
nstan <nstanislass@gmail.com>
2024-10-02 00:51:53 +0300

    move

--- backend/apps/common/permissions/__init__.py
+++ backend/apps/common/permissions/__init__.py
@@ -1,5 +1,1 @@
 from apps.common.permissions.is_owner_or_admin import IsOwnerOrAdmin
-from apps.common.permissions.responses import ACCESS_DENIED_ERROR
-from apps.common.permissions.responses import BAD_REQUEST
-from apps.common.permissions.responses import NOT_FOUND
-from apps.common.permissions.responses import UNAUTHORIZED_ERROR
