7a3b6249406f4289efca31adf4f08141af0e93ca
nstan <nstanislass@gmail.com>
2024-10-01 12:39:01 +0300

    black

--- backend/apps/asgi.py
+++ backend/apps/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.main")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.main')
 
 application = get_asgi_application()
