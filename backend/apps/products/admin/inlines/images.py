28d245b60e68cced80c29ff9547f4c63e12f1f01
nstan <nstanislass@gmail.com>
2024-10-02 00:54:39 +0300

    change get_queryset

--- backend/apps/products/admin/inlines/images.py
+++ backend/apps/products/admin/inlines/images.py
@@ -6,3 +6,7 @@ from apps.products.models import ProductImage
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('product')
