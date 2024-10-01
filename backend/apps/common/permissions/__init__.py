739485e4acfe57b0b92e41525dc9a9c0ae040b13
nstan <nstanislass@gmail.com>
2024-10-01 11:37:58 +0300

    add IsOwnerOrAdmin

--- backend/apps/common/permissions/__init__.py
+++ backend/apps/common/permissions/__init__.py
@@ -1,3 +1,4 @@
+from apps.common.permissions.is_owner_or_admin import IsOwnerOrAdmin
 from apps.common.permissions.responses import ACCESS_DENIED_ERROR
 from apps.common.permissions.responses import BAD_REQUEST
 from apps.common.permissions.responses import NOT_FOUND
