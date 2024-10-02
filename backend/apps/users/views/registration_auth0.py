c6d47136f353cbfdb1e8f88787e78008d0e921b5
nstan <nstanislass@gmail.com>
2024-10-02 12:48:15 +0300

    add example response

--- backend/apps/users/views/registration_auth0.py
+++ backend/apps/users/views/registration_auth0.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext_lazy as _
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiExample
 from rest_framework import response
 from rest_framework import status
 from rest_framework import views
@@ -23,8 +23,23 @@ class UserRegistrationView(views.APIView):
             status.HTTP_201_CREATED: AuthSerializer,
             status.HTTP_400_BAD_REQUEST: responses.BAD_REQUEST,
             status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED_ERROR,
-        },
-    )
+            },
+        examples=[
+            OpenApiExample(
+                name=_('User registration example response'),
+                description=_('Example of a successful response after user registration.'),
+                value={
+                    'email': 'example@example-email.com',
+                    'username': 'example-username',
+                    'tokens': {
+                        'refresh': 'string',
+                        'access': 'string'
+                        }
+                    },
+                response_only=True,
+                )
+            ]
+        )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
