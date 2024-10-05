47854636579a2de9c1809c9739144e69211c7a95
nstan <nstanislass@gmail.com>
2024-10-05 12:21:39 +0300

    add show_full_result_count

--- backend/apps/locations/admin/region.py
+++ backend/apps/locations/admin/region.py
@@ -6,3 +6,4 @@ from apps.locations.models import Region
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'created_at', 'updated_at')
+    show_full_result_count = False
