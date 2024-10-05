2c92386aeda264b8d6dcf755f686c0d3d697c9c5
nstan <nstanislass@gmail.com>
2024-10-05 12:01:51 +0300

    add CitySerializer

---
+++ backend/apps/locations/serializers/city.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from apps.locations.models import City
+from apps.locations.serializers.region import RegionSerializer
+
+
+class CitySerializer(serializers.ModelSerializer):
+    region = RegionSerializer(read_only=True)
+
+    class Meta:
+        model = City
+        fields = ['id', 'name', 'region', 'slug', 'latitude', 'longitude']
