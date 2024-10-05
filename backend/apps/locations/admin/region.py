c13d4691a17ecb47a5d54531b929fe5b6b9d5afc
nstan <nstanislass@gmail.com>
2024-10-05 11:59:28 +0300

    add RegionAdmin

---
+++ backend/apps/locations/admin/region.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from apps.locations.models import Region
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
