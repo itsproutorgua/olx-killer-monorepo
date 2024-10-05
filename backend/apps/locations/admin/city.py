8bfa1390a2de9cf5b1f9687ff598d4e3551dcb99
nstan <nstanislass@gmail.com>
2024-10-05 12:21:54 +0300

    add show_full_result_count, search_fields

--- backend/apps/locations/admin/city.py
+++ backend/apps/locations/admin/city.py
@@ -7,6 +7,8 @@ from apps.locations.models import City
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     list_display = ('name', 'region', 'latitude', 'longitude')
+    search_fields = ['name']
+    show_full_result_count = False
 
     def get_search_fields(self, request):
         language_code = get_language() or 'uk'
