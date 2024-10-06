451164a126a021629b1b024cf7d64f08e8db6f47
nstan <nstanislass@gmail.com>
2024-10-07 00:26:57 +0300

    add get_queryset

--- backend/apps/locations/admin/city.py
+++ backend/apps/locations/admin/city.py
@@ -6,10 +6,14 @@ from apps.locations.models import City
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'region', 'latitude', 'longitude')
+    list_display = ('name', 'region',)
     search_fields = ['name']
     show_full_result_count = False
 
     def get_search_fields(self, request):
         language_code = get_language() or 'uk'
         return [f'name_{language_code}']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('region')
