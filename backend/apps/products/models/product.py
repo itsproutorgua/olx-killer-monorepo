6e179dbdfe9b8811c7f4f5177bbcdb24277d6b2c
nstan <nstanislass@gmail.com>
2024-10-07 00:33:42 +0300

    delete price add params, prod_olx_id

--- backend/apps/products/models/product.py
+++ backend/apps/products/models/product.py
@@ -1,7 +1,4 @@
-from decimal import Decimal
-
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -10,19 +7,10 @@ from apps.common.models import TimestampMixin
 
 User = get_user_model()
 
-MIN_PRICE = Decimal('0.0')
-ERROR_MIN_PRICE_MESSAGE = _(f'Must be greater than or equal to {MIN_PRICE}.')
-
 
 class Product(TimestampMixin, models.Model):
     title = models.CharField(_('Product name'), max_length=255)
-    price = models.DecimalField(
-        _('Price'),
-        max_digits=10,
-        decimal_places=2,
-        validators=[MinValueValidator(MIN_PRICE, message=_(ERROR_MIN_PRICE_MESSAGE))],
-    )
-    description = models.TextField(verbose_name=_('Product description'), blank=True)
+    description = models.TextField(verbose_name=_('Product description'), blank=True, null=True)
     seller = models.ForeignKey(
         to=User,
         on_delete=models.CASCADE,
@@ -35,6 +23,8 @@ class Product(TimestampMixin, models.Model):
         related_name='category_products',
         verbose_name=_('Category'),
     )
+    params = models.JSONField(_('Parameters'), default=dict, blank=True, null=True)
+    prod_olx_id = models.IntegerField('Product OLX ID', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -43,4 +33,4 @@ class Product(TimestampMixin, models.Model):
         db_table = 'product'
         verbose_name = _('Product')
         verbose_name_plural = _('Products')
-        unique_together = ('title', 'seller')
+        unique_together = ('title', 'seller', 'category')
