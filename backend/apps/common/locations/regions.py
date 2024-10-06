0a02805441bdb2500a668e4f40f5042141e63d24
nstan <nstanislass@gmail.com>
2024-10-06 04:10:47 +0300

    add regions

---
+++ backend/apps/common/locations/regions.py
@@ -0,0 +1,140 @@
+regions = {
+    1: {
+        'region': 'Вінницька область',
+        'slug': 'vinnytska-oblast',
+        'languages': {'uk': 'Вінницька область', 'en': 'Vinnytsia Oblast', 'ru': 'Винницкая область'},
+    },
+    2: {
+        'region': 'Волинська область',
+        'slug': 'volynska-oblast',
+        'languages': {'uk': 'Волинська область', 'en': 'Volyn Oblast', 'ru': 'Волынская область'},
+    },
+    3: {
+        'region': 'Дніпропетровська область',
+        'slug': 'dnipropetrovska-oblast',
+        'languages': {
+            'uk': 'Дніпропетровська область',
+            'en': 'Dnipropetrovsk Oblast',
+            'ru': 'Днепропетровская область',
+        },
+    },
+    4: {
+        'region': 'Донецька область',
+        'slug': 'donetska-oblast',
+        'languages': {'uk': 'Донецька область', 'en': 'Donetsk Oblast', 'ru': 'Донецкая область'},
+    },
+    5: {
+        'region': 'Житомирська область',
+        'slug': 'zhytomyrska-oblast',
+        'languages': {'uk': 'Житомирська область', 'en': 'Zhytomyr Oblast', 'ru': 'Житомирская область'},
+    },
+    6: {
+        'region': 'Закарпатська область',
+        'slug': 'zakarpatska-oblast',
+        'languages': {'uk': 'Закарпатська область', 'en': 'Transcarpathian Oblast', 'ru': 'Закарпатская область'},
+    },
+    7: {
+        'region': 'Запорізька область',
+        'slug': 'zaporizka-oblast',
+        'languages': {'uk': 'Запорізька область', 'en': 'Zaporizhzhia Oblast', 'ru': 'Запорожская область'},
+    },
+    8: {
+        'region': 'Івано-Франківська область',
+        'slug': 'ivano-frankivska-oblast',
+        'languages': {
+            'uk': 'Івано-Франківська область',
+            'en': 'Ivano-Frankivsk Oblast',
+            'ru': 'Ивано-Франковская область',
+        },
+    },
+    9: {
+        'region': 'Київська область',
+        'slug': 'kyivska-oblast',
+        'languages': {'uk': 'Київська область', 'en': 'Kyiv Oblast', 'ru': 'Киевская область'},
+    },
+    10: {
+        'region': 'Кіровоградська область',
+        'slug': 'kirovohradska-oblast',
+        'languages': {'uk': 'Кіровоградська область', 'en': 'Kirovohrad Oblast', 'ru': 'Кировоградская область'},
+    },
+    11: {
+        'region': 'Луганська область',
+        'slug': 'luhanska-oblast',
+        'languages': {'uk': 'Луганська область', 'en': 'Luhansk Oblast', 'ru': 'Луганская область'},
+    },
+    12: {
+        'region': 'Львівська область',
+        'slug': 'lvivska-oblast',
+        'languages': {'uk': 'Львівська область', 'en': 'Lviv Oblast', 'ru': 'Львовская область'},
+    },
+    13: {
+        'region': 'Миколаївська область',
+        'slug': 'mykolaivska-oblast',
+        'languages': {'uk': 'Миколаївська область', 'en': 'Mykolaiv Oblast', 'ru': 'Николаевская область'},
+    },
+    14: {
+        'region': 'Одеська область',
+        'slug': 'odeska-oblast',
+        'languages': {'uk': 'Одеська область', 'en': 'Odesa Oblast', 'ru': 'Одесская область'},
+    },
+    15: {
+        'region': 'Полтавська область',
+        'slug': 'poltavska-oblast',
+        'languages': {'uk': 'Полтавська область', 'en': 'Poltava Oblast', 'ru': 'Полтавская область'},
+    },
+    16: {
+        'region': 'Рівненська область',
+        'slug': 'rivnenska-oblast',
+        'languages': {'uk': 'Рівненська область', 'en': 'Rivne Oblast', 'ru': 'Ровенская область'},
+    },
+    17: {
+        'region': 'Сумська область',
+        'slug': 'sumska-oblast',
+        'languages': {'uk': 'Сумська область', 'en': 'Sumy Oblast', 'ru': 'Сумская область'},
+    },
+    18: {
+        'region': 'Тернопільська область',
+        'slug': 'ternopilska-oblast',
+        'languages': {'uk': 'Тернопільська область', 'en': 'Ternopil Oblast', 'ru': 'Тернопольская область'},
+    },
+    19: {
+        'region': 'Харківська область',
+        'slug': 'kharkivska-oblast',
+        'languages': {'uk': 'Харківська область', 'en': 'Kharkiv Oblast', 'ru': 'Харьковская область'},
+    },
+    20: {
+        'region': 'Херсонська область',
+        'slug': 'khersonska-oblast',
+        'languages': {'uk': 'Херсонська область', 'en': 'Kherson Oblast', 'ru': 'Херсонская область'},
+    },
+    21: {
+        'region': 'Хмельницька область',
+        'slug': 'khmelnytska-oblast',
+        'languages': {'uk': 'Хмельницька область', 'en': 'Khmelnytskyi Oblast', 'ru': 'Хмельницкая область'},
+    },
+    22: {
+        'region': 'Черкаська область',
+        'slug': 'cherkaska-oblast',
+        'languages': {'uk': 'Черкаська область', 'en': 'Cherkasy Oblast', 'ru': 'Черкасская область'},
+    },
+    23: {
+        'region': 'Чернівецька область',
+        'slug': 'chernivetska-oblast',
+        'languages': {'uk': 'Чернівецька область', 'en': 'Chernivtsi Oblast', 'ru': 'Черновицкая область'},
+    },
+    24: {
+        'region': 'Чернігівська область',
+        'slug': 'chernihivska-oblast',
+        'languages': {'uk': 'Чернігівська область', 'en': 'Chernihiv Oblast', 'ru': 'Черниговская область'},
+    },
+    25: {
+        'region': 'Місто Київ',
+        'slug': 'kyiv-city',
+        'languages': {'uk': 'Місто Київ', 'en': 'Kyiv City', 'ru': 'Город Киев'},
+    },
+    26: {
+        'region': 'Місто Севастополь',
+        'slug': 'sevastopol-city',
+        'languages': {'uk': 'Місто Севастополь', 'en': 'Sevastopol City', 'ru': 'Город Севастополь'},
+    },
+}
