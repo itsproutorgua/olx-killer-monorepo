9a3657f2bdbebff2e8a9002906610d8163a05f6d
nstan <nstanislass@gmail.com>
2024-10-07 00:38:02 +0300

    import LocationAdmin, VillageAdmin

--- backend/apps/locations/admin/__init__.py
+++ backend/apps/locations/admin/__init__.py
@@ -1,2 +1,4 @@
 from apps.locations.admin.city import CityAdmin
+from apps.locations.admin.location import LocationAdmin
 from apps.locations.admin.region import RegionAdmin
+from apps.locations.admin.village import VillageAdmin
