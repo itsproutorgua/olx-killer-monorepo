fbc2b23bfbbef0f7db7ef566ca5edfbc382e9966
nstan <nstanislass@gmail.com>
2024-10-07 00:37:49 +0300

    import Location, Village

--- backend/apps/locations/models/__init__.py
+++ backend/apps/locations/models/__init__.py
@@ -1,2 +1,4 @@
 from apps.locations.models.city import City
+from apps.locations.models.location import Location
 from apps.locations.models.region import Region
+from apps.locations.models.village import Village
