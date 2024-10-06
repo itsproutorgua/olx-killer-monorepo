48f0f7cffc9a525cb09633b744116f62ece43bab
nstan <nstanislass@gmail.com>
2024-10-07 00:26:05 +0300

    add VillageAdmin

---
+++ backend/apps/locations/admin/village.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.utils.translation import get_language
+
+from apps.locations.models import Village
+
+
+@admin.register(Village)
+class VillageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region',)
+    search_fields = ['name']
+    show_full_result_count = False
+
+    def get_search_fields(self, request):
+        language_code = get_language() or 'uk'
+        return [f'name_{language_code}']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('region')
