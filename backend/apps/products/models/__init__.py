78075493ece997da8e08f06c70bcaea4484de657
nstan <nstanislass@gmail.com>
2024-10-07 00:37:16 +0300

    import price folder

--- backend/apps/products/models/__init__.py
+++ backend/apps/products/models/__init__.py
@@ -1,3 +1,4 @@
 from apps.products.models.category import Category
+from apps.products.models.price import *
 from apps.products.models.product import Product
 from apps.products.models.product_image import ProductImage
