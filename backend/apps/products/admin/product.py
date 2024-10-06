19430a043202296b5bef43089b4e96f839bbb969
nstan <nstanislass@gmail.com>
2024-10-07 00:33:07 +0300

    add PriceInline

--- backend/apps/products/admin/product.py
+++ backend/apps/products/admin/product.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
+from apps.products.admin.inlines import PriceInline
 from apps.products.admin.inlines import ProductImageInline
 from apps.products.models.product import Product
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductImageInline]
+    list_display = ('title', 'seller', 'category')
+    search_fields = ('title',)
+    inlines = [PriceInline, ProductImageInline]
     show_full_result_count = False
 
     def get_queryset(self, request):
