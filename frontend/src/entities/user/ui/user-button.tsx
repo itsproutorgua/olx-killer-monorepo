2ba8f1f1d8a23f63335cbeff34db41ab31f7992f
zim89 <121241841+zim89@users.noreply.github.com>
2024-10-11 21:18:36 +0300

    fixed header styles

--- frontend/src/entities/user/ui/user-button.tsx
+++ frontend/src/entities/user/ui/user-button.tsx
@@ -1,9 +1,9 @@
-import { UserIcon } from '@/shared/ui'
+import { UserRoundedIcon } from '@/shared/ui/icons'
 
 export const UserButton = () => {
   return (
-    <button className='btn-icon'>
-      <UserIcon />
+    <button className='group flex size-11 items-center justify-center text-gray-50 transition-colors duration-300'>
+      <UserRoundedIcon className='h-6 w-6 transition-colors duration-300 group-hover:fill-primary-50' />
     </button>
   )
 }
