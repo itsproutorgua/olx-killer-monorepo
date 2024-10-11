29b0851713fed1f61bf40d5c474824038b114ea4
zim89 <121241841+zim89@users.noreply.github.com>
2024-10-11 21:37:47 +0300

    fixed styles

--- frontend/src/widgets/favorite-button/favorite-button.tsx
+++ frontend/src/widgets/favorite-button/favorite-button.tsx
@@ -1,8 +1,15 @@
+import { cn } from '@/lib/utils'
+
 import { HeartIcon } from '@/shared/ui/icons'
 
-export const FavoriteButton = () => {
+export const FavoriteButton = ({ className }: { className?: string }) => {
   return (
-    <button className='group flex size-11 items-center justify-center text-gray-50 transition-colors duration-300'>
+    <button
+      className={cn(
+        'group flex size-11 items-center justify-center text-gray-50 transition-colors duration-300',
+        className,
+      )}
+    >
       <HeartIcon className='h-6 w-6 fill-primary-900 transition-colors duration-300 group-hover:fill-primary-50' />
     </button>
   )
