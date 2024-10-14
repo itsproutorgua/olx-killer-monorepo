38552a7c5249407cf28a34ae29928ef40968fc8c
nstan <nstanislass@gmail.com>
2024-10-14 04:01:11 +0300

    delete

--- backend/apps/products/serializers/product_image.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from apps.products.models.product_image import ProductImage
-
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image']
