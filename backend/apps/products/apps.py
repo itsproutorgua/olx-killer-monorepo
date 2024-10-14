e3d96b50af2577cc2554d427a67e88bbb76808f3
nstan <nstanislass@gmail.com>
2024-10-14 03:41:35 +0300

    move create_default_currencies

--- backend/apps/products/apps.py
+++ backend/apps/products/apps.py
@@ -1,8 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
-
-from apps.products.utils import DEFAULT_CURRENCIES
 
 
 class ProductsConfig(AppConfig):
@@ -11,17 +8,4 @@ class ProductsConfig(AppConfig):
     verbose_name = _('Products')
 
     def ready(self):
-        import apps.products.translation  # noqa: F401
-        from apps.products.models import Currency  # noqa: F401
-
-        def create_default_currencies(sender, **kwargs):
-            for currency_data in DEFAULT_CURRENCIES:
-                Currency.objects.get_or_create(
-                    code=currency_data['code'],
-                    defaults={
-                        'symbol': currency_data['symbol'],
-                        'name': currency_data['name'],
-                    },
-                )
-
-        post_migrate.connect(create_default_currencies, sender=self)
+        import apps.products.signals  # noqa: F401
