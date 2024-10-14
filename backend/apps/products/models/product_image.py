a376d65fa33a155987d54d82748653100641eec8
nstan <nstanislass@gmail.com>
2024-10-14 04:00:52 +0300

    add MAX_FILE_SIZE_MB add ALLOWED_MIME_TYPES change image change clean

--- backend/apps/products/models/product_image.py
+++ backend/apps/products/models/product_image.py
@@ -1,13 +1,26 @@
-from django.core.files.storage import default_storage
+import mimetypes
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
+from PIL import Image as PILImage
+
+from apps.common import errors
 
 
 class ProductImage(models.Model):
+    MAX_FILE_SIZE_MB = settings.MAX_IMAGE_FILE_SIZE_MB
+    ALLOWED_MIME_TYPES = settings.ALLOWED_IMAGE_MIME_TYPES
+
     product = models.ForeignKey(to='Product', on_delete=models.CASCADE, related_name='product_images')
-    image = models.ImageField(_('Image'), upload_to='products/%Y/%m/%d', null=True, blank=True)
+    image = models.ImageField(
+        _('Image'),
+        upload_to='products/images/%Y/%m/%d',
+        null=True,
+        blank=True,
+        help_text=_('Allowed image formats: %s') % ', '.join(ALLOWED_MIME_TYPES),
+    )
 
     def __str__(self):
         return f"Image for the product '{self.product}'"
@@ -17,11 +30,17 @@ class ProductImage(models.Model):
         verbose_name = _('Product Image')
         verbose_name_plural = _('Product Images')
 
+    def clean(self):
+        super().clean()
+        if self.image:
+            try:
+                with PILImage.open(self.image.file) as img:
+                    mime_type, encoding = mimetypes.guess_type(self.image.name)
+                    if mime_type not in self.ALLOWED_MIME_TYPES:
+                        raise ValidationError(errors.INVALID_IMAGE_TYPE)
+                    img.verify()
+            except Exception:
+                raise ValidationError(errors.INVALID_IMAGE)
 
-@receiver(post_delete, sender=ProductImage)
-def delete_image_file(sender, instance, **kwargs):
-    """Удаляет файл изображения с сервера при удалении ProductImage."""
-    if instance.image:
-        file_path = instance.image.path
-        if default_storage.exists(file_path) and 'examples' not in file_path:
-            default_storage.delete(file_path)
+            if self.image.size > self.MAX_FILE_SIZE_MB * 1024 * 1024:
+                raise ValidationError(errors.IMAGE_SIZE_EXCEEDED)
