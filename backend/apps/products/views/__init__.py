3c68fa289db97c1a1ec01b0c195bb407ee6488aa
nstan <nstanislass@gmail.com>
2024-10-14 04:14:52 +0300

    import ProductFilterViewSet

--- backend/apps/products/views/__init__.py
+++ backend/apps/products/views/__init__.py
@@ -1,4 +1,5 @@
 from apps.products.views.category import CategoryAPIViewSet
 from apps.products.views.currency import CurrencyListView
 from apps.products.views.product import ProductAPIViewSet
+from apps.products.views.product_filters import ProductFilterViewSet
 from apps.products.views.tmp_product import TMPProductCreateAPIView
