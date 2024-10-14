4120f3687399d8ad8200828f00a01ff172fb0703
nstan <nstanislass@gmail.com>
2024-10-14 04:04:11 +0300

    add signals

---
+++ backend/apps/products/signals.py
@@ -0,0 +1,46 @@
+import os
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from apps.products.models import Product
+from apps.products.models import ProductImage
+from apps.products.models import ProductVideo
+from apps.products.tasks import delete_product_file
+
+
+@receiver(post_delete, sender=ProductImage)
+def delete_image_file(sender, instance, **kwargs):
+    """Removes the image file from the server when ProductImage is deleted using Celery task."""
+    if instance.image:
+        file_path = instance.image.path
+        if os.path.exists(file_path) and 'examples' not in file_path:
+            delete_product_file.delay(file_path)
+
+
+@receiver(post_delete, sender=Product)
+def delete_product_images(sender, instance, **kwargs):
+    """Removes image files from the server when a Product is deleted."""
+    images = instance.product_images.all()
+    print(images)
+    for image in images:
+        if image.image:
+            file_path = image.image.path
+            if os.path.exists(file_path) and 'examples' not in file_path:
+                delete_product_file.delay(file_path)
+        image.delete()
+
+
+@receiver(post_delete, sender=ProductVideo)
+def delete_video_file(sender, instance, **kwargs):
+    """Removes the video file from the server when ProductVideo is uninstalled."""
+    if instance.video:
+        instance.video.delete(save=False)
+
+
+@receiver(post_delete, sender=Product)
+def delete_related_videos(sender, instance, **kwargs):
+    """Removes all associated videos when deleting a product."""
+    product_videos = ProductVideo.objects.filter(product=instance)
+    for video in product_videos:
+        video.delete()
