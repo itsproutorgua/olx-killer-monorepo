f8592650f69b8d8c33c1069a8ec7964580abc644
nstan <nstanislass@gmail.com>
2024-10-14 04:02:47 +0300

    add Profile

---
+++ backend/apps/users/models/profile.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.common.models import TimestampMixin
+from apps.locations.models import Location
+
+
+class Profile(TimestampMixin, models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile', verbose_name=_('User'))
+    picture = models.ImageField(
+        _('profile picture'),
+        upload_to='user_pictures/',
+        blank=True,
+        null=True,
+        help_text=_('Upload a profile picture.'),
+    )
+    location = models.ForeignKey(
+        to=Location,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='users',
+        verbose_name=_('Location'),
+        help_text=_('This field allows you to specify the user\'s city and region.'),
+    )
+    phone_numbers = models.JSONField(
+        _('Phone numbers'), default=list, blank=True, null=True, help_text=_('You can add multiple phone numbers.')
+    )
+    is_fake_user = models.BooleanField(_('fake user'), default=False)
+    user_olx_id = models.IntegerField('USER OLX ID', blank=True, null=True)
+
+    def __str__(self):
+        return str(f'Profile for {self.user}')
+
+    class Meta:
+        db_table = 'user_profile'
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
