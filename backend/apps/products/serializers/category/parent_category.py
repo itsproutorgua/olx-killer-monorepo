e66da339f63d7f47a2f0ffc2d5839cb702ad5b84
nstan <nstanislass@gmail.com>
2024-10-14 03:53:12 +0300

    add extend_schema_field add fields path, views delete field id

--- backend/apps/products/serializers/category/parent_category.py
+++ backend/apps/products/serializers/category/parent_category.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from apps.products.models import Category
@@ -8,8 +9,9 @@ class ParentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ('id', 'title', 'parent')
+        fields = ('title', 'path', 'views', 'parent')
 
+    @extend_schema_field(serializers.DictField)
     def get_parent(self, category: Category) -> dict | None:
         if category.parent:
             return ParentSerializer(category.parent).data
