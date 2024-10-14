1114a5de7c18092e69708f3d88fb31b173ed5176
nstan <nstanislass@gmail.com>
2024-10-14 03:43:32 +0300

    change list add retrieve

--- backend/apps/products/views/category.py
+++ backend/apps/products/views/category.py
@@ -1,44 +1,27 @@
+from django.core.cache import cache
+from django.db.models import F
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
 from drf_spectacular.utils import extend_schema
-from drf_spectacular.utils import extend_schema_view
-from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.utils import OpenApiResponse
 from rest_framework import mixins
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from apps.api_tags import CATEGORY_TAG
-from apps.common import responses
+from apps.common import errors
 from apps.common.utils import get_sort_field_by_language
 from apps.products.models import Category
 from apps.products.serializers import CategorySerializer
 
 
-@extend_schema_view(
-    list=extend_schema(
-        tags=[CATEGORY_TAG],
-        summary=_('List all categories'),
-        description=_('Retrieve a list of all categories'),
-        responses={
-            status.HTTP_200_OK: CategorySerializer(many=True),
-        },
-    ),
-    retrieve=extend_schema(
-        tags=[CATEGORY_TAG],
-        summary=_('Retrieve a category by ID'),
-        description=_('Retrieve a single category by ID'),
-        responses={
-            status.HTTP_200_OK: CategorySerializer,
-            status.HTTP_404_NOT_FOUND: responses.CATEGORY_NOT_FOUND,
-        },
-    ),
-)
 class CategoryAPIViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
-    queryset = Category.objects.all().select_related('parent')
     serializer_class = CategorySerializer
     permission_classes = (AllowAny,)
+    queryset = Category.objects.all().select_related('parent').prefetch_related('children')
+    lookup_field = 'path'
 
     def get_queryset(self):
         queryset = self.queryset
@@ -48,36 +31,33 @@ class CategoryAPIViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, Gener
 
     @extend_schema(
         tags=[CATEGORY_TAG],
-        summary=_('List root categories'),
-        description=_('Retrieve a list of root categories (where parent is None)'),
-        responses={status.HTTP_200_OK: CategorySerializer(many=True)},
+        summary=_('List categories in a tree structure'),
+        description=_('Retrieve a hierarchical tree of categories.'),
+        responses={
+            status.HTTP_200_OK: CategorySerializer(many=True),
+        },
     )
-    @action(detail=False, methods=['get'], url_path='roots')
-    def list_root_categories(self, request):
-        """Получить категории, у которых родитель None"""
-        root_categories = self.get_queryset().filter(parent__isnull=True)
-        serializer = self.get_serializer(root_categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def list(self, request, *args, **kwargs):
+        cache_key = 'category_tree'
+        category_tree = cache.get(cache_key)
+
+        if not category_tree:
+            category_tree = Category.get_categories_tree()
+            cache.set(cache_key, category_tree, timeout=900)
+
+        return Response(category_tree, status=status.HTTP_200_OK)
 
     @extend_schema(
         tags=[CATEGORY_TAG],
-        summary=_('List child categories'),
-        description=_('Retrieve a list of child categories by parent ID'),
-        parameters=[
-            OpenApiParameter(name='parent_id', description=_('ID of the parent category'), required=True, type=int)
-        ],
+        summary=_('Retrieve a category by path'),
+        description=_('Retrieve a single category by path'),
         responses={
-            status.HTTP_200_OK: CategorySerializer(many=True),
-            status.HTTP_404_NOT_FOUND: responses.INVALID_PARENT_CATEGORY,
+            status.HTTP_200_OK: CategorySerializer,
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(description=errors.CATEGORY_NOT_FOUND),
         },
     )
-    @action(detail=False, methods=['get'], url_path='children')
-    def list_child_categories(self, request):
-        """Получить категории, у которых родитель имеет указанный ID"""
-        parent_id = request.query_params.get('parent_id')
-        if not parent_id or not parent_id.isdigit():
-            return Response({'error': responses.INVALID_PARENT_CATEGORY}, status=status.HTTP_400_BAD_REQUEST)
-
-        child_categories = self.get_queryset().filter(parent_id=int(parent_id))
-        serializer = self.get_serializer(child_categories, many=True)
+    def retrieve(self, request, path=None):
+        category = get_object_or_404(Category, path=path.strip('/'))
+        Category.objects.filter(id=category.id).update(views=F('views') + 1)
+        serializer = self.serializer_class(category)
         return Response(serializer.data, status=status.HTTP_200_OK)
