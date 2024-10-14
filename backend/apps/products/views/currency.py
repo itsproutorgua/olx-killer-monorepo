60672cf1b26aa552772117b8783e2bdb9d1bcb55
nstan <nstanislass@gmail.com>
2024-10-14 03:47:40 +0300

    add CurrencyListView

---
+++ backend/apps/products/views/currency.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics
+
+from apps.api_tags import CURRENCY
+from apps.products.models import Currency
+from apps.products.serializers.price import CurrencySerializer
+
+
+@extend_schema(
+    tags=[CURRENCY],
+    summary=_('Retrieve a list of all currencies'),
+    description=_('Returns a list of available currencies with their code, symbol, and name.'),
+    responses={200: CurrencySerializer(many=True)},
+)
+class CurrencyListView(generics.ListAPIView):
+    queryset = Currency.objects.all().order_by('id')
+    serializer_class = CurrencySerializer
