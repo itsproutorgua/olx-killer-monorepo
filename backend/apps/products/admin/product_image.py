24731167006829eef9870fb678496c62a4df49a7
nstan <nstanislass@gmail.com>
2024-10-14 03:59:23 +0300

    delete

--- backend/apps/products/admin/product_image.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from apps.products.models import ProductImage
-
-
-@admin.register(ProductImage)
-class ProductImageAdmin(admin.ModelAdmin):
-    show_full_result_count = False
