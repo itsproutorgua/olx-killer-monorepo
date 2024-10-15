09a60b7b6f2cdfe44a8dd3e27d87b423e56f507d
nstan <nstanislass@gmail.com>
2024-10-16 01:26:50 +0300

    add product_count, readonly_fields

--- backend/apps/products/admin/category.py
+++ backend/apps/products/admin/category.py
@@ -1,17 +1,34 @@
 from django.contrib import admin
+from django.db.models import Count
 from modeltranslation.admin import TranslationAdmin
 
+from apps.products.admin.filters import PopularCategoryFilter
 from apps.products.models import Category
 
 
 @admin.register(Category)
 class CategoryAdmin(TranslationAdmin):
-    list_display = ('title', 'parent', 'updated_at', 'created_at')
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = ('id', 'title', 'parent', 'views', 'product_count', 'updated_at', 'created_at')
+    list_display_links = ('id', 'title')
+    readonly_fields = (
+        'views',
+        'slug',
+        'path',
+        'cat_id_olx',
+        'created_at',
+        'updated_at',
+    )
+    autocomplete_fields = ['parent']
     search_fields = ('title', 'parent__title')
-    list_filter = ('title',)
+    list_filter = (PopularCategoryFilter,)
+
     show_full_result_count = False
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
-        return queryset.select_related('parent')
+        queryset = queryset.annotate(product_count=Count('products'))
+        return queryset.select_related('parent').prefetch_related('products').order_by('-product_count')
+
+    @admin.display(description='Количество продуктов', ordering='product_count')
+    def product_count(self, obj):
+        return obj.product_count
