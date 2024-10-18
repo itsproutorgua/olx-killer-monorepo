63f5fa537f5f34db6f6fbca80d1dde6de7486fd2
nstan <nstanislass@gmail.com>
2024-10-18 14:02:58 +0300

    change fieldsets

--- backend/apps/users/admin/user.py
+++ backend/apps/users/admin/user.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.core.exceptions import ValidationError
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 
@@ -10,17 +12,48 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = (
-        'email',
-        'is_email_verified',
-        'is_staff',
-        'is_superuser',
-        'is_active',
-        'date_joined',
+    fieldsets = (
+        (None, {"fields": ('profile_link', "username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    readonly_fields = ('last_login', 'date_joined')
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ('email', 'profile_link', 'is_email_verified', 'date_joined')
+    readonly_fields = ('profile_link', 'last_login', 'date_joined')
     list_display_links = ('email',)
     show_full_result_count = False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('profile').order_by('-updated_at')
+
+    @admin.display(description=_('Profile link'))
+    def profile_link(self, obj):
+        if hasattr(obj, 'profile'):
+            profile_link_text = _('View Profile')
+            profile_url = reverse('admin:users_profile_change', args=[obj.profile.id])
+            profile_link_html = f'<a href="{profile_url}">{profile_link_text}</a>'
+            return format_html(profile_link_html)
+        return '-'
 
     def save_model(self, request, obj, form, change):
         password1 = form.cleaned_data.get('password1')
