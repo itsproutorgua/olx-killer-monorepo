c8e2319ee568bc5d8fe640fa4a4faae0b7bf52a3
nstan <nstanislass@gmail.com>
2024-10-18 14:02:23 +0300

    change title name user_email

--- backend/apps/users/admin/profile.py
+++ backend/apps/users/admin/profile.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from apps.users.models import Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user__email', 'created_at', 'updated_at')
-    list_display_links = (
-        'id',
-        'user__email',
-    )
+    list_display = ('user_email', 'created_at', 'updated_at')
+    list_display_links = ('user_email',)
     search_fields = ('user__email',)
     readonly_fields = ('user', 'user_olx_id', 'is_fake_user', 'created_at', 'updated_at')
     autocomplete_fields = ('location',)
 
     def get_queryset(self, request):
-        return super().get_queryset(request).select_related('user').order_by('user__email')
+        return super().get_queryset(request).select_related('user').order_by('-updated_at')
+
+    def has_add_permission(self, request):
+        return False
+
+    @admin.display(description=_('User Email'), ordering='user__email')
+    def user_email(self, obj):
+        return obj.user.email
