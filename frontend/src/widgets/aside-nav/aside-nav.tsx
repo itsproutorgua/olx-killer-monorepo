7ae06fc82bbb5589d07c220fbc0027af78d12c16
Demirov Vlad <demirov.vlad@gmail.com>
2024-10-18 20:48:07 +0300

    backend server protocol changed to https, gitignore modified

--- frontend/src/widgets/aside-nav/aside-nav.tsx
+++ frontend/src/widgets/aside-nav/aside-nav.tsx
@@ -1,6 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
 import { ChevronRight } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+
+import { categoryApi } from '@/entities/category'
+import { CategoryResponse } from '@/entities/category/model/category.types.ts'
 import {
   Menubar,
   MenubarContent,
@@ -10,72 +14,79 @@ import {
 } from '@/shared/ui/shadcn-ui/menubar.tsx'
 import { Separator } from '@/shared/ui/shadcn-ui/separator'
 import { FacebookIconOutline, InstagramIconOutline } from '@/shared/ui'
-import { categoryApi } from "@/entities/category";
-import { useQuery } from "@tanstack/react-query";
-import { QUERY_KEYS } from "@/shared/constants";
-import {CategoryResponse } from "@/entities/category/model/category.types.ts";
-import AsideCategoryLoader from "@/shared/ui/loaders/aside-category.loader.tsx";
+import AsideCategoryLoader from '@/shared/ui/loaders/aside-category.loader.tsx'
+import { QUERY_KEYS } from '@/shared/constants'
 
 export const AsideNav = () => {
-  const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation()
 
-  const mainUrl = 'http://olx.erpsolutions.com.ua:8000/';
+  const mainUrl = 'https://olx.erpsolutions.com.ua:8000/'
 
-  const { isLoading, isError, data: categories, error } = useQuery<CategoryResponse[]>({
+  const {
+    isLoading,
+    isError,
+    data: categories,
+    error,
+  } = useQuery<CategoryResponse[]>({
     queryKey: [QUERY_KEYS.CATEGORIES, i18n.language],
     queryFn: () => categoryApi.findAll(1),
     retry: 1,
     refetchOnWindowFocus: false,
-  });
+  })
 
-  if (isError) return <p>Error: {error.message}</p>;
+  if (isError) return <p>Error: {error.message}</p>
 
   // Render the categories
   return (
     <aside className='w-[305px] pl-[18px] pr-2'>
       <Menubar className='h-auto rounded-none border-0 p-0'>
-        {isLoading && <ul className="space-y-2.5 max-w-[263px]">
-          {Array.from({length: 13}).map((_, index) => (
-            <li key={index}>
-              <AsideCategoryLoader/> {/* Render 13 loaders */}
-            </li>
-          ))}
-        </ul>}
-        <ul className="space-y-2.5 max-w-[276px]">
-          {categories?.map((cat) => (
+        {isLoading && (
+          <ul className='max-w-[263px] space-y-2.5'>
+            {Array.from({ length: 13 }).map((_, index) => (
+              <li key={index}>
+                <AsideCategoryLoader /> {/* Render 13 loaders */}
+              </li>
+            ))}
+          </ul>
+        )}
+        <ul className='max-w-[276px] space-y-2.5'>
+          {categories?.map(cat => (
             <li key={cat.path}>
               <MenubarMenu key={cat.path}>
-                <MenubarTrigger
-                  className='group flex w-full cursor-pointer items-center justify-between gap-3 rounded-[81px] border-0 py-0 pl-2 pr-2 text-base/4 font-normal transition-colors duration-300 hover:bg-primary-900 hover:text-gray-50 data-[state=open]:bg-primary-900 data-[state=open]:text-gray-50'>
+                <MenubarTrigger className='group flex w-full cursor-pointer items-center justify-between gap-3 rounded-[81px] border-0 py-0 pl-2 pr-2 text-base/4 font-normal transition-colors duration-300 hover:bg-primary-900 hover:text-gray-50 data-[state=open]:bg-primary-900 data-[state=open]:text-gray-50'>
                   {cat.icon ? (
                     <img
                       src={mainUrl + cat.icon}
                       alt={cat.title}
-                      className='w-6 h-6 transition duration-300 ease-in-out group-hover:filter group-hover:invert group-hover:brightness-0 group-data-[state=open]:filter group-data-[state=open]:invert group-data-[state=open]:brightness-0 focus:filter focus:invert focus:brightness-0'
+                      className='h-6 w-6 transition duration-300 ease-in-out focus:brightness-0 focus:invert focus:filter group-hover:brightness-0 group-hover:invert group-hover:filter group-data-[state=open]:brightness-0 group-data-[state=open]:invert group-data-[state=open]:filter'
                     />
                   ) : null}
-                  <p className='flex-1 text-start text-ellipsis whitespace-nowrap overflow-hidden hover:overflow-visible'>{cat.title}</p>
+                  <p className='flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-start hover:overflow-visible'>
+                    {cat.title}
+                  </p>
                   <ChevronRight />
                 </MenubarTrigger>
                 <MenubarContent
                   side='right'
                   sideOffset={15}
-                  className='max-w-[900px] grid grid-cols-3 gap-[50px] rounded-[15px] border-none bg-background px-[75px] py-12 text-foreground shadow-[1px_1px_5px_0_rgba(78,78,78,0.19)]'
+                  className='grid max-w-[900px] grid-cols-3 gap-[50px] rounded-[15px] border-none bg-background px-[75px] py-12 text-foreground shadow-[1px_1px_5px_0_rgba(78,78,78,0.19)]'
                 >
-                  {cat.children.slice(0, 6).map((sub) => (
+                  {cat.children.slice(0, 6).map(sub => (
                     <div key={sub.path} className='max-w-[155px] space-y-5'>
                       <Link to={`/${sub.path}`}>
-                        <h4 className='text-base/[19.36px] font-semibold hover:text-primary-600 text-ellipsis whitespace-nowrap overflow-hidden hover:overflow-visible'>
+                        <h4 className='overflow-hidden text-ellipsis whitespace-nowrap text-base/[19.36px] font-semibold hover:overflow-visible hover:text-primary-600'>
                           {sub.title}
                         </h4>
                       </Link>
 
                       <ul className='space-y-2.5'>
-                        {sub.children.slice(0, 5).map((item) => (
+                        {sub.children.slice(0, 5).map(item => (
                           <li key={item.path}>
                             <Link to={`/${item.path}`}>
                               <MenubarItem className='max-w-[155px] cursor-pointer bg-none p-0 text-base/[19.36px] font-normal transition-colors duration-300 focus:text-primary-600'>
-                                <p className="flex-1 text-start text-ellipsis whitespace-nowrap overflow-hidden hover:whitespace-nowrap hover:overflow-visible">{item.title}</p>
+                                <p className='flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-start hover:overflow-visible hover:whitespace-nowrap'>
+                                  {item.title}
+                                </p>
                               </MenubarItem>
                             </Link>
                           </li>
@@ -89,13 +100,12 @@ export const AsideNav = () => {
           ))}
         </ul>
       </Menubar>
-      <div className='flex items-center justify-center pt-[19px] pb-[26px] pr-[43px]'>
+      <div className='flex items-center justify-center pb-[26px] pr-[43px] pt-[19px]'>
         <Separator className='bg-gray-200' />
       </div>
       <div className='flex flex-col'>
         <p className='flex-1 pl-2.5'>{t('asideLinks.socialMedia')}</p>
-        <div
-          className='flex w-[271px] items-center gap-0 rounded-[81px] py-0.5'>
+        <div className='flex w-[271px] items-center gap-0 rounded-[81px] py-0.5'>
           <a
             href='#'
             aria-label='Instagram'
@@ -113,5 +123,5 @@ export const AsideNav = () => {
         </div>
       </div>
     </aside>
-  );
-};
+  )
+}
--- frontend/src/widgets/category-popular/category-popular.tsx
+++ frontend/src/widgets/category-popular/category-popular.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
-import { useMediaQuery } from '@/shared/library/hooks'
 import { CollapsibleContent } from '@radix-ui/react-collapsible'
+import { useQuery } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
-import {categoryApi} from '@/entities/category';
+import { categoryApi } from '@/entities/category'
+import { CategoryResponse } from '@/entities/category/model/category.types.ts'
 import {
   Collapsible,
   CollapsibleTrigger,
 } from '@/shared/ui/shadcn-ui/collapsible'
 import { SectionTitle } from '@/shared/ui'
-import {useQuery} from "@tanstack/react-query";
-import {CategoryResponse} from "@/entities/category/model/category.types.ts";
-import {QUERY_KEYS} from "@/shared/constants";
-import PopularCategoryLoader from "@/shared/ui/loaders/popular-categories.loader.tsx";
+import PopularCategoryLoader from '@/shared/ui/loaders/popular-categories.loader.tsx'
+import { QUERY_KEYS } from '@/shared/constants'
+import { useMediaQuery } from '@/shared/library/hooks'
 
 export const PopularCategories = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -22,16 +22,21 @@ export const PopularCategories = () => {
 
   const { t, i18n } = useTranslation()
 
-  const mainUrl = 'http://olx.erpsolutions.com.ua:8000/';
+  const mainUrl = 'https://olx.erpsolutions.com.ua:8000/'
 
-  const { isLoading, isError, data: categories, error } = useQuery<CategoryResponse[]>({
+  const {
+    isLoading,
+    isError,
+    data: categories,
+    error,
+  } = useQuery<CategoryResponse[]>({
     queryKey: [QUERY_KEYS.CATEGORIES, i18n.language],
     queryFn: () => categoryApi.findAll(1),
     retry: 1,
     refetchOnWindowFocus: false,
-  });
+  })
 
-  if (isError) return <p>Error: {error.message}</p>;
+  if (isError) return <p>Error: {error.message}</p>
 
   return (
     <section className='pt-[78px] xl:pt-[116px]'>
@@ -40,28 +45,34 @@ export const PopularCategories = () => {
 
         <Collapsible open={isOpen} onOpenChange={setIsOpen}>
           <div>
-            {isLoading && <ul className='grid grid-cols-2 gap-x-2.5 gap-y-[45px] xl:grid-cols-7 xl:gap-x-[38px]'>
-              {Array.from({length: 14}).map((_, index) => (
-                <li key={index}>
-                  <PopularCategoryLoader/>
-                </li>
-              ))}
-            </ul>}
+            {isLoading && (
+              <ul className='grid grid-cols-2 gap-x-2.5 gap-y-[45px] xl:grid-cols-7 xl:gap-x-[38px]'>
+                {Array.from({ length: 14 }).map((_, index) => (
+                  <li key={index}>
+                    <PopularCategoryLoader />
+                  </li>
+                ))}
+              </ul>
+            )}
             <ul className='grid grid-cols-2 gap-x-2.5 gap-y-[45px] xl:grid-cols-7 xl:gap-x-[38px]'>
               {categories?.slice(0, isDesktop ? 13 : 6).map(cat => (
                 <li
                   key={cat.path}
                   className='cursor-pointer transition duration-300 hover:scale-105'
                 >
-                  <Link to={`/${cat.path}`} reloadDocument className='space-y-[15px]'>
-                    <div className="size-[172px] rounded-full xl:size-[150px]">
+                  <Link
+                    to={`/${cat.path}`}
+                    reloadDocument
+                    className='space-y-[15px]'
+                  >
+                    <div className='size-[172px] rounded-full xl:size-[150px]'>
                       <img
                         src={mainUrl + cat.img}
                         alt={cat.title}
-                        className="rounded-full"
+                        className='rounded-full'
                       />
                     </div>
-                    <h3 className="line-clamp-1 text-center text-base/[20.8px] hover:overflow-visible">
+                    <h3 className='line-clamp-1 text-center text-base/[20.8px] hover:overflow-visible'>
                       {cat.title}
                     </h3>
                   </Link>
@@ -69,20 +80,22 @@ export const PopularCategories = () => {
               ))}
             </ul>
             <CollapsibleContent>
-              <ul className="mt-[45px] grid grid-cols-2 gap-x-2.5 gap-y-8 xl:grid-cols-7 xl:gap-x-[38px]">
-              {categories?.slice(isDesktop ? 13 : 6, categories.length).map(cat => (
+              <ul className='mt-[45px] grid grid-cols-2 gap-x-2.5 gap-y-8 xl:grid-cols-7 xl:gap-x-[38px]'>
+                {categories
+                  ?.slice(isDesktop ? 13 : 6, categories.length)
+                  .map(cat => (
                     <li
                       key={cat.path}
                       className='cursor-pointer space-y-[15px] transition duration-300 hover:scale-105'
                     >
-                      <div className="size-[172px] rounded-full xl:size-[150px]">
+                      <div className='size-[172px] rounded-full xl:size-[150px]'>
                         <img
                           src={mainUrl + cat.img}
                           alt={cat.title}
-                          className="rounded-full"
+                          className='rounded-full'
                         />
                       </div>
-                      <h3 className="line-clamp-1 text-center text-base/[20.8px]">
+                      <h3 className='line-clamp-1 text-center text-base/[20.8px]'>
                         {cat.title}
                       </h3>
                     </li>
@@ -91,11 +104,13 @@ export const PopularCategories = () => {
             </CollapsibleContent>
           </div>
 
-          {(categories && categories.length > (isDesktop ? 14 : 6)) && <CollapsibleTrigger className='w-full mt-[42px] rounded-[60px] border border-border py-[13px] text-center text-[13px]/[13px] transition-colors duration-300 hover:bg-primary-500 hover:text-gray-50 xl:text-base/4'>
-            {isOpen
-              ? `${t('buttons.showLess')}`
-              : `${t('buttons.showAllCategories')}`}
-          </CollapsibleTrigger>}
+          {categories && categories.length > (isDesktop ? 14 : 6) && (
+            <CollapsibleTrigger className='border-border mt-[42px] w-full rounded-[60px] border py-[13px] text-center text-[13px]/[13px] transition-colors duration-300 hover:bg-primary-500 hover:text-gray-50 xl:text-base/4'>
+              {isOpen
+                ? `${t('buttons.showLess')}`
+                : `${t('buttons.showAllCategories')}`}
+            </CollapsibleTrigger>
+          )}
         </Collapsible>
       </div>
     </section>
