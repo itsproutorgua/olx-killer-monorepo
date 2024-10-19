4876db80661983283be9576f25c811f474984f83
Vladyslav Demirov <96706666+demirov-vlad@users.noreply.github.com>
2024-10-19 16:59:58 +0300

    api changed to main server (#7)
    * api changed to main server
    
    * type fixes

--- frontend/src/widgets/aside-nav/aside-nav.tsx
+++ frontend/src/widgets/aside-nav/aside-nav.tsx
@@ -20,8 +20,6 @@ import { QUERY_KEYS } from '@/shared/constants'
 export const AsideNav = () => {
   const { t, i18n } = useTranslation()
 
-  const mainUrl = 'https://olx.erpsolutions.com.ua:8000/'
-
   const {
     isLoading,
     isError,
@@ -56,7 +54,7 @@ export const AsideNav = () => {
                 <MenubarTrigger className='group flex w-full cursor-pointer items-center justify-between gap-3 rounded-[81px] border-0 py-0 pl-2 pr-2 text-base/4 font-normal transition-colors duration-300 hover:bg-primary-900 hover:text-gray-50 data-[state=open]:bg-primary-900 data-[state=open]:text-gray-50'>
                   {cat.icon ? (
                     <img
-                      src={mainUrl + cat.icon}
+                      src={cat.icon}
                       alt={cat.title}
                       className='h-6 w-6 transition duration-300 ease-in-out focus:brightness-0 focus:invert focus:filter group-hover:brightness-0 group-hover:invert group-hover:filter group-data-[state=open]:brightness-0 group-data-[state=open]:invert group-data-[state=open]:filter'
                     />
