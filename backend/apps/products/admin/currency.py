c476c0d9cf83bea2dafe3a82cb14ad480e3f7637
nstan <nstanislass@gmail.com>
2024-10-29 18:51:29 +0200

    change ModelAdmin -> SimpleHistoryAdmin

--- backend/apps/products/admin/currency.py
+++ backend/apps/products/admin/currency.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from apps.products.models import Currency
 
 
 @admin.register(Currency)
-class CurrencyAdmin(admin.ModelAdmin):
+class CurrencyAdmin(SimpleHistoryAdmin):
     list_display = ('code', 'name', 'symbol')
     fields = ('code', 'name', 'symbol', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
