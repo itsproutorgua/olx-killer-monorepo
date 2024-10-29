48672cf7acfa34738b644289f83c9787c61de881
nstan <nstanislass@gmail.com>
2024-10-29 19:26:14 +0200

    add file_exists_on_s3

---
+++ backend/apps/products/utils/check_file_exists.py
@@ -0,0 +1,29 @@
+import boto3
+from botocore.exceptions import ClientError
+from django.conf import settings
+
+from apps.log_config import logger
+
+
+s3_client = boto3.client('s3')
+
+
+def file_exists_on_s3(key: str) -> bool:
+    """Check if a file exists in S3.
+
+    Args:
+        key (str): The S3 key of the file.
+
+    Returns:
+        bool: True if the file exists, False otherwise.
+    """
+    try:
+        s3_client.head_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
+        return True
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            logger.warning(f'File not found on S3: {key}')
+            return False
+        else:
+            logger.error(f'Error checking file on S3: {str(e)}')
+            return False
