511999ee8922cd17f626e7777e10a1ad6ee80e36
nstan <nstanislass@gmail.com>
2024-10-29 18:51:02 +0200

    delete username

--- backend/apps/users/management/commands/create_default_superuser.py
+++ backend/apps/users/management/commands/create_default_superuser.py
@@ -8,12 +8,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = settings.SUPER_LOGIN.split('@')[0]
         email = settings.SUPER_LOGIN
         password = settings.SUPER_PASSWORD
 
         if User.objects.filter(email=email).exists():
             self.stdout.write(self.style.WARNING('User with this email already exists.'))
         else:
-            User.objects.create_superuser(username=username, email=email, password=password)
+            User.objects.create_superuser(email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser {email}'))
