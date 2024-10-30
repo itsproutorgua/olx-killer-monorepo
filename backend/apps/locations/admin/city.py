f27bdacfc3a7614948441fed945841d1878afee8
nstan <nstanislass@gmail.com>
2024-10-31 00:08:22 +0200

    add SimpleHistoryAdmin

--- backend/apps/locations/admin/city.py
+++ backend/apps/locations/admin/city.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.utils.translation import get_language
+from simple_history.admin import SimpleHistoryAdmin
 
 from apps.locations.models import City
 
 
 @admin.register(City)
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(SimpleHistoryAdmin):
     list_display = ('name', 'region')
     readonly_fields = ('created_at', 'updated_at')
     search_fields = ['name']
--- backend/apps/locations/admin/region.py
+++ backend/apps/locations/admin/region.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from apps.locations.models import Region
 
 
 @admin.register(Region)
-class RegionAdmin(admin.ModelAdmin):
+class RegionAdmin(SimpleHistoryAdmin):
     list_display = ('name', 'slug', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
     show_full_result_count = False
