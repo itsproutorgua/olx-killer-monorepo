9675113bfd238361b632048331cbe5675bf279c2
nstan <nstanislass@gmail.com>
2024-10-30 12:00:40 +0200

    add check_and_set_default_image

--- backend/apps/products/models/category.py
+++ backend/apps/products/models/category.py
@@ -4,13 +4,12 @@ from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 from slugify import slugify
 
-from apps.common.models import HistoricalModel
 from apps.common.models import TimestampMixin
 from apps.products.utils import translate_and_set_fields
 from apps.products.utils.check_file_exists import file_exists_on_s3
 
 
-class Category(TimestampMixin, HistoricalModel, models.Model):
+class Category(TimestampMixin,  models.Model):
     title = models.CharField(_('Category name'), max_length=100)
     parent = models.ForeignKey(
         'self',
@@ -67,26 +66,25 @@ class Category(TimestampMixin, HistoricalModel, models.Model):
         return [{**values} for cat_id, values in categories_dict.items() if values['parent_id'] is None]
 
     def save(self, *args, **kwargs):
-
         if not self.slug and self.title:
             self.slug = slugify(self.title)
 
-        if self.parent:
-            self.path = f'{self.parent.path}/{self.slug}'
-        else:
-            self.path = f'{self.slug}'
+        self.path = f'{self.parent.path}/{self.slug}' if self.parent else self.slug
 
         if self.title:
             translate_and_set_fields(self, field_name_prefix='title', field_to_translate='title')
 
-        if self.parent is None and not self.img:
-            img_key = f'{settings.AWS_LOCATION}/categories/images/{self.slug}.png'
-            if file_exists_on_s3(img_key):
-                self.img = img_key
-
-        if self.parent is None and not self.icon:
-            icon_key = f'{settings.AWS_LOCATION}/categories/icons/{self.slug}.svg'
-            if file_exists_on_s3(icon_key):
-                self.icon = icon_key
+        if self.parent is None:
+            self.img = self.check_and_set_default_image(self.slug, 'images')
+            self.icon = self.check_and_set_default_image(self.slug, 'icons')
 
         super().save(*args, **kwargs)
+
+    @staticmethod
+    def check_and_set_default_image(slug: str, image_type: str) -> str | None:
+        file_path = f'categories/{image_type}/{slug}'
+        image_key = f'{file_path}.png' if image_type == 'images' else f'{file_path}.svg'
+
+        if file_exists_on_s3(f'{settings.AWS_LOCATION}/{image_key}'):
+            return image_key
+        return None
