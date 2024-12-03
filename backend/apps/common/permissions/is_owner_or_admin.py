ac381cd9d8edaec4a5f166b9740e8edf85986526
nstan <nstanislass@gmail.com>
2024-12-03 03:43:45 +0200

    change IsOwnerOrAdmin

--- backend/apps/common/permissions/is_owner_or_admin.py
+++ backend/apps/common/permissions/is_owner_or_admin.py
@@ -3,11 +3,11 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    Разрешение, которое позволяет редактировать или удалять продукт только создателю или администратору.
+    Permission that allows only the creator or administrator to edit or delete a product.
     """
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return obj.seller == request.user or request.user.is_staff
+        user = getattr(obj, 'user', None) or getattr(obj, 'seller')
+        return user == request.user or request.user.is_staff
