efbc9568625d1a2e404f8ea6ff15090440606e7a
Demirov Vlad <demirov.vlad@gmail.com>
2024-12-13 21:25:40 +0200

    removed backend-registration-api and bugs/styles fixes

--- frontend/src/shared/ui/skeletons/popular-category-skeleton.tsx
+++ frontend/src/shared/ui/skeletons/popular-category-skeleton.tsx
@@ -4,12 +4,12 @@ import { useMediaQuery } from '@/shared/library/hooks'
 export const PopularCategorySkeleton = () => {
   const isDesktop = useMediaQuery('(min-width: 1440px)')
   return (
-    <ul className='flex items-center gap-[16px] px-1 pt-1 xl:mb-[86px] xl:grid xl:grid-cols-7 xl:gap-x-[38px] xl:gap-y-[45px] xl:pb-0'>
+    <ul className='flex items-center gap-[16px] px-1 xl:mb-[92px] xl:grid xl:grid-cols-7 xl:gap-x-[38px] xl:gap-y-[45px]'>
       {Array.from({ length: isDesktop ? 7 : 6 }).map((_, index) => (
         <li key={index}>
           <PopularCategoryLoader
             width={isDesktop ? 150 : 70}
-            height={isDesktop ? 185.8 : 119}
+            height={isDesktop ? 205 : 108}
           />
         </li>
       ))}
