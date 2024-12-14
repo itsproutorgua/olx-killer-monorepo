3061cc5e78ac054c64c453d8add054e27934a32e
nstan <nstanislass@gmail.com>
2024-12-14 05:12:54 +0200

    add cascade_delete_history

--- backend/apps/common/models/history.py
+++ backend/apps/common/models/history.py
@@ -3,7 +3,7 @@ from simple_history.models import HistoricalRecords
 
 
 class HistoricalModel(models.Model):
-    history = HistoricalRecords(inherit=True)
+    history = HistoricalRecords(inherit=True, cascade_delete_history=True)
 
     class Meta:
         abstract = True
