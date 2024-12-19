77707d0bb69aa05261796189ddb3bfdee6dc7e37
nstan <nstanislass@gmail.com>
2024-12-19 08:11:25 +0200

    add ProductByUserSerializer

---
+++ backend/apps/products/serializers/product/product_by_user.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from apps.products.models.product import Product
+from apps.products.serializers.price.price import PriceSerializer
+from apps.products.serializers.product.product_image import ProductImageSerializer
+
+
+class ProductByUserSerializer(serializers.ModelSerializer):
+    prices = PriceSerializer(many=True)
+    images = ProductImageSerializer(many=True, source='product_images')
+
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'title',
+            'prices',
+            'images',
+            'active',
+            'slug',
+            'views',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at', 'slug', 'views']
