6bbc5ef59f48f16cdf3b608feaabfa0e67e0154d
nstan <nstanislass@gmail.com>
2024-12-20 08:44:38 +0200

    import ProductActivityFilter, PopularSellerFilter, ProductStatusFilter

--- backend/apps/products/admin/filters/__init__.py
+++ backend/apps/products/admin/filters/__init__.py
@@ -1,1 +1,4 @@
+from apps.products.admin.filters.active_inactive import ProductActivityFilter
 from apps.products.admin.filters.popular_category import PopularCategoryFilter
+from apps.products.admin.filters.popular_seller import PopularSellerFilter
+from apps.products.admin.filters.status import ProductStatusFilter
