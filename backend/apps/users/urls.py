5b9b8958130244fab4102bd6debe164acc354aa6
nstan <nstanislass@gmail.com>
2024-12-20 08:45:37 +0200

    change users/products/ -> users/profile/products/

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -7,5 +7,5 @@ app_name = 'api_users'
 
 urlpatterns = [
     path('users/profile/', views.ProfileRetrieveUpdateDeleteView.as_view(), name='profile'),
-    path('users/products/', views.UserProductsView.as_view(), name='user-products'),
+    path('users/profile/products/', views.UserProductsView.as_view(), name='user-products'),
 ]
