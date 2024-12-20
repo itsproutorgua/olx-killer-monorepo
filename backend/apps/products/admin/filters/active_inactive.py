3a6c86d359c3604044eb9f52fc052d6839150bc9
nstan <nstanislass@gmail.com>
2024-12-20 09:20:52 +0200

    change lookups

--- backend/apps/products/admin/filters/active_inactive.py
+++ backend/apps/products/admin/filters/active_inactive.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ProductActivityFilter(SimpleListFilter):
-    title = _('Active Status')
+    title = _('Activity Status')
     parameter_name = 'active'
 
     def lookups(self, request, model_admin):
         return [
-            ('active', _('Active products')),
-            ('inactive', _('Inactive products')),
+            ('active', _('Active')),
+            ('inactive', _('Inactive')),
         ]
 
     def queryset(self, request, queryset):
