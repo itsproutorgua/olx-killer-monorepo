ec521fc17c233b7711d3cd727d305601646bde88
Demirov Vlad <demirov.vlad@gmail.com>
2024-12-27 22:36:29 +0200

    account page sidebar icons changed

--- frontend/src/shared/constants/account-sidebar.const.ts
+++ frontend/src/shared/constants/account-sidebar.const.ts
@@ -1,33 +1,53 @@
+import { FavoritesIconOutline } from '@/shared/ui/icons/favorites-icon-outline.tsx'
 import { FavoritesIcon } from '@/shared/ui/icons/favorites-icon.tsx'
+import { MessagesIconOutline } from '@/shared/ui/icons/messages-icon-outline.tsx'
 import { MessagesIcon } from '@/shared/ui/icons/messages-icon.tsx'
+import { PenIconRoundedOutline } from '@/shared/ui/icons/pen-icon-rounded-outline.tsx'
 import { PenIconRounded } from '@/shared/ui/icons/pen-icon-rounded.tsx'
+import { ProfileIconOutline } from '@/shared/ui/icons/profile-icon-outline.tsx'
 import { ProfileIcon } from '@/shared/ui/icons/profile-icon.tsx'
+import { SettingsIconOutline } from '@/shared/ui/icons/settings-icon-outline.tsx'
 import { SettingsIcon } from '@/shared/ui/icons/settings-icon.tsx'
 
 export const SIDEBAR_ITEMS = [
   {
     title: 'account.listings',
     url: '/account/listings',
-    icon: PenIconRounded,
+    icon: {
+      solid: PenIconRounded,
+      outline: PenIconRoundedOutline,
+    },
   },
   {
     title: 'account.messages',
     url: '/account/chat',
-    icon: MessagesIcon,
+    icon: {
+      solid: MessagesIcon,
+      outline: MessagesIconOutline,
+    },
   },
   {
     title: 'account.favorites',
     url: '/account/favorites',
-    icon: FavoritesIcon,
+    icon: {
+      solid: FavoritesIcon,
+      outline: FavoritesIconOutline,
+    },
   },
   {
     title: 'account.profile',
     url: '/account/profile',
-    icon: ProfileIcon,
+    icon: {
+      solid: ProfileIcon,
+      outline: ProfileIconOutline,
+    },
   },
   {
     title: 'account.settings',
     url: '/account/settings',
-    icon: SettingsIcon,
+    icon: {
+      solid: SettingsIcon,
+      outline: SettingsIconOutline,
+    },
   },
 ]
