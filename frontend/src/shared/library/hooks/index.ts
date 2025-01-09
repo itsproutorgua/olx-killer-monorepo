056becb5a493df27e121ba37d1b8af3e6c640585
zim89 <121241841+zim89@users.noreply.github.com>
2025-01-09 18:55:10 +0200

    del unused hook

--- frontend/src/shared/library/hooks/index.ts
+++ frontend/src/shared/library/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useMediaQuery } from './use-media-query'
 export { useQueryParams } from './use-query-params'
-export { useIntersection } from './use-intersection'
 export { useStrictContext, createStrictContext } from './use-strict-context'
