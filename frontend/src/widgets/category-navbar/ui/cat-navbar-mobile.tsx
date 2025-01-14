14c8f18d1dc23d5d969ed4de04b608180697f08a
zim89 <121241841+zim89@users.noreply.github.com>
2025-01-14 19:23:55 +0200

    refactor cat-navbar-mobile

--- frontend/src/widgets/category-navbar/ui/cat-navbar-mobile.tsx
+++ frontend/src/widgets/category-navbar/ui/cat-navbar-mobile.tsx
@@ -1,63 +1,61 @@
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
 import type { CategoryChild } from '@/entities/category'
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/shared/ui/shadcn-ui/collapsible'
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/shared/ui/shadcn-ui/carousel'
+import { ChevronRight } from '@/shared/ui/icons'
 import { PUBLIC_PAGES } from '@/shared/constants'
-import { CategoryNavbarItem } from './cat-navbar-item'
 
 export const CategoryNavbarMobile = ({ data }: { data: CategoryChild[] }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const { t } = useTranslation()
+  const items = Math.ceil(data.length / 9)
+  console.log(items)
 
   return (
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className='space-y-5 md:hidden'
-    >
-      <div>
-        <ul className='grid grid-cols-2 border-t border-t-gray-200'>
-          {data.slice(0, 6).map(item => {
-            const href = `${PUBLIC_PAGES.CATALOG}/${item.path}`
+    <Carousel>
+      <div className='space-y-4'>
+        <CarouselContent className='-ml-5'>
+          {Array.from({ length: items }).map((_, idx) => (
+            <CarouselItem
+              key={idx}
+              className='basis-[250px] border-r border-r-border pl-5 pr-5'
+            >
+              <ul className='space-y-[14px]'>
+                {data.slice(idx * 9, (idx + 1) * 9).map(item => {
+                  const href = `${PUBLIC_PAGES.CATALOG}/${item.path}`
 
-            return (
-              <li
-                key={item.path}
-                className='border-b border-gray-200 odd:border-r'
-              >
-                <CategoryNavbarItem item={item} href={href} />
-              </li>
-            )
-          })}
-        </ul>
-
-        <CollapsibleContent>
-          <ul className='grid grid-cols-2'>
-            {data.slice(6).map(item => {
-              const href = `${PUBLIC_PAGES.CATALOG}/${item.path}`
-
-              return (
-                <li
-                  key={item.path}
-                  className='border-b border-gray-200 odd:border-r'
-                >
-                  <CategoryNavbarItem item={item} href={href} />
-                </li>
-              )
-            })}
-          </ul>
-        </CollapsibleContent>
+                  return (
+                    <li key={item.path} className='text-sm font-medium'>
+                      {item.title.length > 20 ? (
+                        <Link to={href} className='flex gap-1'>
+                          <span className='line-clamp-1'>{item.title}</span>
+                          <span>({item.products_cumulative_count})</span>
+                        </Link>
+                      ) : (
+                        <Link to={href}>
+                          {item.title} ({item.products_cumulative_count})
+                        </Link>
+                      )}
+                    </li>
+                  )
+                })}
+              </ul>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className='flex w-full justify-between'>
+          <CarouselPrevious className='static m-4 flex size-7 translate-y-0 rotate-180 items-center justify-center border-none bg-primary-900 text-white !opacity-100 disabled:bg-gray-200'>
+            <ChevronRight />
+          </CarouselPrevious>
+          <CarouselNext className='static m-4 flex size-7 translate-y-0 items-center justify-center border-none bg-primary-900 text-white !opacity-100 disabled:bg-gray-200'>
+            <ChevronRight />
+          </CarouselNext>
+        </div>
       </div>
-      {data.length > 6 && (
-        <CollapsibleTrigger className='btn-secondary'>
-          {isOpen ? t('buttons.showLess') : t('buttons.showMore')}
-        </CollapsibleTrigger>
-      )}
-    </Collapsible>
+    </Carousel>
   )
 }
