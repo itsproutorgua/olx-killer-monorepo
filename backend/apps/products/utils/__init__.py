60a37441934bfa899a061e16f7d06e5a7cdf392d
nstan <nstanislass@gmail.com>
2025-01-17 16:00:24 +0200

    update

--- backend/apps/products/utils/__init__.py
+++ backend/apps/products/utils/__init__.py
@@ -1,4 +1,5 @@
+from apps.products.utils.currency_enum import CurrencyEnum
 from apps.products.utils.translation_utils import translate_and_set_fields
 
 
-__all__ = ['translate_and_set_fields']
+__all__ = ['translate_and_set_fields', 'CurrencyEnum']
