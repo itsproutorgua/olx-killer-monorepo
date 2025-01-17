7d7c15db934eab97605f2a39628c9cb81e7df8ba
nstan <nstanislass@gmail.com>
2025-01-17 16:01:37 +0200

    add CurrencyEnum

---
+++ backend/apps/products/utils/currency_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class CurrencyEnum(Enum):
+    UAH = {'code': 'UAH', 'symbol': '₴', 'name': 'Ukrainian Hryvnia'}
+    USD = {'code': 'USD', 'symbol': '$', 'name': 'United States Dollar'}
