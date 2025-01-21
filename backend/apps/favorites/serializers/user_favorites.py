481b4a11491a3d40227acdb1cbed8c403f322014
nstan <nstanislass@gmail.com>
2025-01-21 23:37:18 +0200

    delete get_price add prices

--- backend/apps/favorites/serializers/user_favorites.py
+++ backend/apps/favorites/serializers/user_favorites.py
@@ -2,7 +2,6 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from apps.favorites.models import Favorite
-from apps.log_config import logger
 from apps.products.models import Product
 from apps.products.serializers.price.price import PriceSerializer
 from apps.products.serializers.product.product_image import ProductImageSerializer
@@ -41,23 +40,9 @@ class UserFavoriteSerializer(serializers.ModelSerializer):
         return {
             'id': obj.product.id,
             'title': obj.product.title,
-            'price': self.get_price(obj),
+            'prices': PriceSerializer(obj.product.prices.all(), many=True).data,
             'images': ProductImageSerializer(obj.product.product_images.all(), many=True).data,
             'slug': obj.product.slug,
             'updated_at': obj.product.updated_at,
             'created_at': obj.product.created_at,
         }
-
-    def get_price(self, obj) -> str:
-        prices = PriceSerializer(obj.product.prices.all(), many=True).data
-        current_language = self.context['request'].LANGUAGE_CODE
-        current_currency = SUPPORTED_LANGUAGES_TO_CURRENCIES.get(current_language, 'UAH')
-
-        for price in prices:
-            if (code := price.get('currency')['code']) == current_currency:
-                amount = price.get('amount')
-                currency_code = code
-                return f'{amount} {currency_code}'
-
-        logger.error(f'Could not find price for {obj.product}')
-        return _('Price not available')
