389b3b44c8e2294220323ba4a4a2753a6de81df8
nstan <nstanislass@gmail.com>
2025-01-26 14:05:07 +0200

    add ValidationError for len location_name

--- backend/apps/locations/views/location.py
+++ backend/apps/locations/views/location.py
@@ -5,6 +5,7 @@ from drf_spectacular.utils import extend_schema
 from drf_spectacular.utils import OpenApiParameter
 from drf_spectacular.utils import OpenApiResponse
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 
@@ -18,8 +19,8 @@ from apps.locations.serializers.location import LocationSerializer
     summary=_('Retrieve a list of locations'),
     description=_(
         'This endpoint returns a list of locations (cities and villages) ordered alphabetically by city name. '
-        'Each location includes its id, name, location_type, region, latitude, and longitude. '
-        'You can optionally filter locations by their name using the "location_name" query parameter.'
+        'Each location includes its `id`, `name`, `location_type`, `region`, `latitude`, and `longitude`. '
+        'You can optionally filter locations by their name using the `location_name` query parameter.'
     ),
     parameters=[
         OpenApiParameter(
@@ -45,7 +46,6 @@ class LocationListView(ListAPIView):
     queryset = Location.objects
     serializer_class = LocationSerializer
     permission_classes = (AllowAny,)
-    lookup_field = 'city__name'
 
     def get_queryset(self):
         language_code = self.request.LANGUAGE_CODE
@@ -58,6 +58,9 @@ class LocationListView(ListAPIView):
         )
         # fmt: on
         location_name = self.request.query_params.get('location_name', None)
+        if len(location_name) < 3:
+            raise ValidationError(_('Location name must be at least 3 characters long.'))
+
         if location_name:
             city_field_name = f'city__name_{language_code}'
             # village_field_name = f'village__name_{language_code}'
