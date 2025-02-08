67aedf15b470d6afd5c4d05ce9a2d8f7eaea6a2b
nstan <nstanislass@gmail.com>
2025-02-08 15:58:50 +0200

    update

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -10,7 +10,7 @@ worker_connections = 1000
 timeout = 30
 
 # Настройки для логирования Gunicorn
-loglevel = 'info'  # Уровень логирования
+loglevel = 'debug'  # Уровень логирования
 
 # Настройки формата для запросов Gunicorn
 access_log_format = '%(t)s %(h)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
