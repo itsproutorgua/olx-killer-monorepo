f7c5c0a42dae3f2e0f848e31651c42d9c9e7348e
nstan <nstanislass@gmail.com>
2025-02-08 14:40:06 +0200

    update

--- backend/settings/logging.py
+++ backend/settings/logging.py
@@ -10,8 +10,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            'datefmt': '%A-%d-%B-%Y  %H:%M:%S',
+            'format': '%(asctime)s | %(levelname)s | %(name)s  | %(message)s',
+            'datefmt': '%d-%m-%Y  %H:%M:%S',
         },
     },
     'handlers': {
