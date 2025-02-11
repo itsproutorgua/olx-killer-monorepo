0325efff808244a173e360b056dcce8481eb2876
solonko_anton <asolonko42@gmail.com>
2025-02-11 22:15:27 +0000

    updated asgi.py

--- backend/apps/asgi.py
+++ backend/apps/asgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '..settings.main')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
@@ -18,8 +22,7 @@ from django.core.asgi import get_asgi_application
 from .chat.routing import websocket_urlpatterns
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '..settings.main')
-django.setup()
+
 
 
 application = ProtocolTypeRouter(
