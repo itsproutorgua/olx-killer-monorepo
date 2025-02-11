98f7bceab4ee88f3ac4b8bdbed6153834c3d6f68
nstan <nstanislass@gmail.com>
2025-02-11 14:53:59 +0200

    change default_request user for test

--- backend/settings/main.py
+++ backend/settings/main.py
@@ -100,7 +100,7 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': ('rest_framework.throttling.UserRateThrottle',),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '200/day',
-        'user': '1000/day',
+        'user': '1000000000/day',
     },
 }
 
