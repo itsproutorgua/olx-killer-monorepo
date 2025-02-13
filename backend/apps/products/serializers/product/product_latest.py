9db17133ce49598a0d8b0382c60b9fffc4c71043
nstan <nstanislass@gmail.com>
2025-02-13 09:53:09 +0200

    add video

--- backend/apps/products/serializers/product/product_latest.py
+++ backend/apps/products/serializers/product/product_latest.py
@@ -3,11 +3,13 @@ from rest_framework import serializers
 from apps.products.models.product import Product
 from apps.products.serializers.price.price import PriceSerializer
 from apps.products.serializers.product.product_image import ProductImageSerializer
+from apps.products.serializers.product.product_video import ProductVideoSerializer
 
 
 class LatestProductSerializer(serializers.ModelSerializer):
     prices = PriceSerializer(many=True)
     images = ProductImageSerializer(many=True, source='product_images')
+    video = ProductVideoSerializer(source='product_videos', read_only=True)
 
     class Meta:
         model = Product
@@ -16,6 +18,7 @@ class LatestProductSerializer(serializers.ModelSerializer):
             'title',
             'prices',
             'description',
+            'video',
             'images',
             'slug',
             'created_at',
