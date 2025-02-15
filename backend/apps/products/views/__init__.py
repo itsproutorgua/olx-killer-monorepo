8b4633104def520089bc9e0a58832dc740edb732
nstan <nstanislass@gmail.com>
2025-02-15 22:16:41 +0200

    add ProductSearchViewSet

--- backend/apps/products/views/__init__.py
+++ backend/apps/products/views/__init__.py
@@ -2,6 +2,7 @@ from apps.products.views.category import CategoryAPIViewSet
 from apps.products.views.currency import CurrencyListView
 from apps.products.views.product import ProductAPIViewSet
 from apps.products.views.product_filters import ProductFilterViewSet
+from apps.products.views.product_search import ProductSearchViewSet
 from apps.products.views.products_latest import LatestProductListView
 from apps.products.views.tmp_product import TMPProductCreateAPIView
 
@@ -12,5 +13,6 @@ __all__ = [
     'ProductAPIViewSet',
     'ProductFilterViewSet',
     'LatestProductListView',
+    'ProductSearchViewSet',
     'TMPProductCreateAPIView',
 ]
