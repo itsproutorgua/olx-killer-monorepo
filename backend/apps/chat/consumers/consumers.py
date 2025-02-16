f17ff2aa931b7f540627007456faa915f49ecc60
solonko_anton <asolonko42@gmail.com>
2025-02-16 22:34:39 +0000

    added messages chack and refactored code

--- backend/apps/chat/consumers/consumers.py
+++ backend/apps/chat/consumers/consumers.py
@@ -1,122 +1,87 @@
 import json
 
-from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.core.exceptions import FieldError
-from rest_framework.exceptions import AuthenticationFailed
+from messages_operations import (chat_message, mark_message_as_delivered, 
+                                  mark_message_as_read, serialize_message,
+                                  get_message, save_message, send_last_messages)
 
-from apps.users.authentication import Auth0JWTAuthentication
+from users_and_rooms_operations import (update_user_status, get_chat_room, 
+                                         authenticate_user, is_user_in_chat, 
+                                         get_recipient, is_user_online)
 
-from ..models.chat import ChatRoom
-from ..models.message import Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # get room id
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.chat_group_name = f'chat_{self.room_id}'
+        
+        # Check if chat room exists
+        self.room = await get_chat_room(self.room_id)
+        if not self.room:
+            return await self.close()
 
-        # get room
-        self.room = await sync_to_async(
-            lambda: ChatRoom.objects.filter(id=self.room_id).first(), thread_sensitive=True
-        )()
+        # Authenticate user
+        self.scope['user'] = await authenticate_user(self.scope)
+        if not self.scope['user']:
+            return await self.close()
 
-        if self.room is None:
-            await self.close()
-
-        # get token from headers
-        headers = dict(self.scope.get('headers'))
-        token = (
-            headers.get(b'authorization', b'').decode('utf-8').split(' ')[1] if b'authorization' in headers else None
-        )
-
-        # vaildate token
-        if not token:
-            raise AuthenticationFailed('Authorization token is missing')
-
-        auth0_auth = Auth0JWTAuthentication()
-
-        try:
-            validated_token = auth0_auth.get_validated_token(token)
-            self.scope['user'] = await sync_to_async(auth0_auth.get_user)(validated_token)
-        except AuthenticationFailed as e:
-            await self.close()
-            raise e
-
-        # get user emails from room
-        self.users = await sync_to_async(
-            lambda: list(
-                ChatRoom.objects.select_related('first_user', 'second_user')
-                .filter(id=self.room_id)
-                .values_list('first_user__email', 'second_user__email')
-            ),
-            thread_sensitive=True,
-        )()
-
-        if not self.users:
-            await self.close()
-
-        user1_email, user2_email = self.users[0]
-
-        # check if connected user is in the room
-        if self.scope['user'].email not in {user1_email, user2_email}:
-            await self.close()
-
+        # Check if user is in chat
+        if not await is_user_in_chat(self.room_id, self.scope['user'].email):
+            return await self.close()
+        
         # Connect to chat group
-        await self.channel_layer.group_add(
-            self.chat_group_name,
-            self.channel_name,
-        )
-
+        await self.channel_layer.group_add(self.chat_group_name, self.channel_name)
         await self.accept()
 
-        # Load 50 last messages from database
-        try:
-            messages = await sync_to_async(
-                lambda: list(
-                    Message.objects.filter(chat_room=self.room)
-                    .select_related('sender')
-                    .values('text', 'sender__email')
-                    .order_by('-created_at')[:50]
-                ),
-                thread_sensitive=True,
-            )()
-        except FieldError:
-            messages = []
+        # Sending last messages
+        await send_last_messages(self)
 
-        messages_data = [{'text': msg['text'], 'sender_email': msg['sender__email']} for msg in messages]
-
-        await self.send(text_data=json.dumps(messages_data))
+        # Update user status
+        await update_user_status(self.scope['user'], is_online=True)
 
     async def disconnect(self, close_code):
-        # Disconnect from chat group
-        await self.channel_layer.group_discard(
-            self.chat_group_name,
-            self.channel_name,
+        # Leave chat group
+        await self.channel_layer.group_discard(self.chat_group_name, self.channel_name)
+
+        # Update user status
+        await update_user_status(self.scope['user'], is_online=False)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_text = data.get('message')
+        if not message_text:
+            return
+        
+        # Save message to database
+        message = await save_message(self, message_text)
+        
+        # Send message to chat group
+        await self.channel_layer.group_send(
+            self.chat_group_name, {
+                'type': 'chat_message',
+                'message_id': message.id,
+            }
         )
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        test_data_json = json.loads(text_data)
-        message = test_data_json['message']
-
-        # Save message to database
-        await sync_to_async(
-            lambda: Message.objects.create(
-                chat_room=self.room,
-                sender=self.scope['user'],
-                text=message,
-            ),
-            thread_sensitive=True,
-        )()
-
-        # Send message to chat group
-        await self.channel_layer.group_send(self.chat_group_name, {'type': 'chat_message', 'message': message})
-
-    # Receive message from chat group
     async def chat_message(self, event):
-        message = event['message']
+        await chat_message(self, event)
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({'message': message}))
+    async def mark_message_as_delivered(self, message_id):
+        await mark_message_as_delivered(self, message_id)
+
+    async def mark_message_as_read(self, message_id):
+        await mark_message_as_read(self, message_id)
+
+    async def serialize_message(self, message):
+        return await serialize_message(message)
+
+    async def get_message(self, message_id):
+        return await get_message(message_id)
+
+    async def get_recipient(self):
+        return await get_recipient(self.room, self.scope['user'])
+
+    async def is_user_online(self, user):
+        return await is_user_online(user)
+    
