2a489c88dcb0289939ae5c7973c1eba115d9900b
solonko_anton <asolonko42@gmail.com>
2025-02-19 21:56:58 +0000

    updated urls

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from apps.chat.views.chat import ChatRoomView
+from apps.chat.views.message import DeleteMessageView
+from apps.chat.views.message import EditMessageView
 
 
 router = SimpleRouter()
@@ -13,4 +15,6 @@ app_name = 'chat'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('messages/<int:message_id>/delete/', DeleteMessageView.as_view(), name='delete_message'),
+    path('messages/<int:message_id>/edit/', EditMessageView.as_view(), name='edit_message'),
 ]
