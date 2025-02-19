664c94423a04bfc2dc4c45b9d08be8a1578fda1e
solonko_anton <asolonko42@gmail.com>
2025-02-19 21:54:50 +0000

    added exceptions

---
+++ backend/apps/chat/exceptions/exceptions.py
@@ -0,0 +1,18 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class InvalidTokenFormatError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Invalid token format."
+    default_code = "invalid_token_format"
+
+class MalformedAuthorizationHeaderError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Malformed authorization header."
+    default_code = "malformed_authorization_header"
+
+class DatabaseIntegrityError(APIException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = "Database integrity error during authentication."
+    default_code = "database_integrity_error"
