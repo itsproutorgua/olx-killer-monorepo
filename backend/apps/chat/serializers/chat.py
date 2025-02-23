c1feb88b5ee058074b21ed33b5f2bfa2914e9db3
solonko_anton <asolonko42@gmail.com>
2025-02-23 10:45:08 +0000

    updated ChatSerializer

--- backend/apps/chat/serializers/chat.py
+++ backend/apps/chat/serializers/chat.py
@@ -2,16 +2,12 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from apps.chat.models.chat import ChatRoom
-from apps.chat.serializers.message import MessageSerializer
 
 
 User = get_user_model()
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True, read_only=True)
-    users = serializers.SlugRelatedField(many=True, queryset=User.objects.all(), slug_field='username')
-
     class Meta:
         model = ChatRoom
-        fields = ['id', 'users', 'messages']
+        fields = ['id', 'first_user', 'second_user']
