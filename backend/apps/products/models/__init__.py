b49e8cc3dbc718e07aa9f29005bc4cf6a657ad7c
nstan <nstanislass@gmail.com>
2025-02-23 13:20:11 +0200

    update

--- backend/apps/products/models/__init__.py
+++ backend/apps/products/models/__init__.py
@@ -1,8 +1,9 @@
 from apps.products.models.category import Category
+from apps.products.models.deactivate_feedback import ProductDeactivationFeedback
 from apps.products.models.price import *
 from apps.products.models.product import Product
 from apps.products.models.product_image import ProductImage
 from apps.products.models.product_video import ProductVideo
 
 
-__all__ = ['Category', 'Product', 'ProductImage', 'ProductVideo', 'Currency', 'Price']
+__all__ = ['Category', 'Product', 'ProductImage', 'ProductVideo', 'ProductDeactivationFeedback', 'Currency', 'Price']
