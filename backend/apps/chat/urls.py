b970562281d3a9336aab9249d0c2a6ae3ef1f4b5
solonko_anton <asolonko42@gmail.com>
2025-02-24 17:18:16 +0000

    updated urls

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from apps.chat.views.chat import ChatRoomView
+from apps.chat.views.chat import ChatRoomCreateView
+from apps.chat.views.chat import ChatRoomRetrieveView
 
 
 app_name = 'chat'
 
+
 urlpatterns = [
-    path('chat-rooms/get_or_create_room/', ChatRoomView.as_view(), name='get_or_create_room'),
+    path('chat-rooms/create/', ChatRoomCreateView.as_view(), name='create-room'),
+    path('chat-rooms/retrieve/', ChatRoomRetrieveView.as_view(), name='retrieve-room'),
 ]
