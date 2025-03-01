44c501c61ff84010a9377b1793197850c20ebf1a
solonko_anton <asolonko42@gmail.com>
2025-03-01 01:10:51 +0000

    updated middlwares

--- backend/apps/chat/middlwares/queryparams.py
+++ backend/apps/chat/middlwares/queryparams.py
@@ -1,11 +1,12 @@
-from channels.middleware import BaseMiddleware
 from urllib.parse import parse_qs
+
+from channels.middleware import BaseMiddleware
 
 
 class QueryParamsMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         scope = dict(scope)
 
-        scope["query_params"] = parse_qs(scope["query_string"].decode())
+        scope['query_params'] = parse_qs(scope['query_string'].decode())
 
         return await super().__call__(scope, receive, send)
