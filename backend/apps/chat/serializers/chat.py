2f35f7e177eb647e894c77f1df1fbb21dc05dba1
solonko_anton <asolonko42@gmail.com>
2025-03-01 01:10:21 +0000

    added serializers

---
+++ backend/apps/chat/serializers/chat.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from apps.chat.models.chat import ChatRoom
+
+
+class ChatRecieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatRoom
+        fields = ['id', 'first_user', 'second_user']
