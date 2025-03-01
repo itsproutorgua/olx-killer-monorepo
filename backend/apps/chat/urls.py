9ff4b3a9174195e4989ef4c17d3b832cde3fa65e
solonko_anton <asolonko42@gmail.com>
2025-03-01 01:08:27 +0000

    updated urls in chat

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+
 from apps.chat.views.chat import ChatRecieveView
+
 
 app_name = 'chat'
 
-urlpatterns = [
-    path('chat/recieve/', ChatRecieveView.as_view({'get': 'list'}), name='chat-recieve')
-]
+urlpatterns = [path('chat/recieve/', ChatRecieveView.as_view({'get': 'list'}), name='chat-recieve')]
