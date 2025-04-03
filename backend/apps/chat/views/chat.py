696cff2eaed915277ebe3fe812444a769ed62042
solonko-anton <asolonko42@gmail.com>
2025-04-03 14:31:33 +0000

    updated

--- backend/apps/chat/views/chat.py
+++ backend/apps/chat/views/chat.py
@@ -8,6 +8,7 @@ from rest_framework.viewsets import GenericViewSet
 from apps.chat.models.chat import ChatRoom
 from apps.chat.models.message import Message
 from apps.chat.serializers.chat import ChatReceiveSerializer
+from apps.users.models.profile import Profile
 
 
 class ChatsReceiveView(ListModelMixin, GenericViewSet):
@@ -15,13 +16,16 @@ class ChatsReceiveView(ListModelMixin, GenericViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.GET.get('first_user')
+        profile_id = self.request.GET.get('profile_id')
+        print(f'Profile ID: {profile_id}')
+        profile = Profile.objects.filter(id=profile_id).first()
+        print(f'Profile: {profile}')
         last_message_subquery = (
             Message.objects.filter(chat_room=OuterRef('pk')).order_by('created_at').values('created_at')[:1]
         )
 
         return (
-            ChatRoom.objects.filter(Q(first_user=user) | Q(second_user=user))
+            ChatRoom.objects.filter(Q(first_user=profile.user) | Q(second_user=profile.user))
             .annotate(last_message_time=Subquery(last_message_subquery))
             .order_by('last_message_time')
         )
