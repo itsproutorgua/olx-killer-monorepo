29281245dcd97413f82ef3706341023a748b64f5
solonko-anton <asolonko42@gmail.com>
2025-05-12 15:31:15 +0000

    init attachment model

--- backend/apps/chat/models/__init__.py
+++ backend/apps/chat/models/__init__.py
@@ -1,6 +1,7 @@
 from apps.chat.models.chat import ChatRoom
 from apps.chat.models.message import Message
 from apps.chat.models.useractivity import UserActivity
+from apps.chat.models.attachment import Attachment
 
 
-__all__ = ['ChatRoom', 'Message', 'UserActivity']
+__all__ = ['ChatRoom', 'Message', 'UserActivity', 'Attachment']
