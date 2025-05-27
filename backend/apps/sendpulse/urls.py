9df255d5d12ae826cc207a56df16b7bcde3384bd
solonko-anton <asolonko42@gmail.com>
2025-05-27 17:10:31 +0000

    code refactored, fixed permissions

--- backend/apps/sendpulse/urls.py
+++ backend/apps/sendpulse/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
+
 from apps.sendpulse.views.views import SaveElectronInkData
 
 
 app_name = 'sendpulse'
 
-urlpatterns = [
-    path('save/electronink/', SaveElectronInkData.as_view(), name="save_data_from_electronink")
-]
+urlpatterns = [path('save/electronink/', SaveElectronInkData.as_view(), name='save_data_from_electronink')]
