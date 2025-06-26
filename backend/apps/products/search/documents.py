7e3831b736b3e1a5cbc50399f512739c9d8e6efc
solonko-anton <asolonko42@gmail.com>
2025-06-26 16:02:33 +0000

    fixed search

--- backend/apps/products/search/documents.py
+++ backend/apps/products/search/documents.py
@@ -4,38 +4,14 @@ from apps.products.models.product import Product
 
 @registry.register_document
 class ProductDocument(Document):
-    title = fields.TextField(analyzer="multilang_analyzer")
-    description = fields.TextField(analyzer="multilang_analyzer")
-
+    title = fields.TextField(analyzer="standard")
+    description = fields.TextField(analyzer="standard")
+    
     class Index:
         name = "products"
         settings = {
             'number_of_shards': 1,
-            'number_of_replicas': 1,
-            'analysis': {
-                'analyzer': {
-                    'multilang_analyzer': {
-                        'type': 'custom',
-                        'tokenizer': 'standard',
-                        'char_filter': ['html_strip'],  
-                        'filter': [
-                            'lowercase',
-                            'russian_stop',
-                            'ukrainian_stop',
-                            'english_stop',
-                            'russian_stemmer',
-                            'english_stemmer'
-                        ]
-                    }
-                },
-                'filter': {
-                    'russian_stop': {'type': 'stop', 'stopwords': '_russian_'},
-                    'ukrainian_stop': {'type': 'stop', 'stopwords': '_ukrainian_'},
-                    'english_stop': {'type': 'stop', 'stopwords': '_english_'},
-                    'russian_stemmer': {'type': 'stemmer', 'language': 'russian'},
-                    'english_stemmer': {'type': 'stemmer', 'language': 'english'}
-                }
-            }
+            'number_of_replicas': 1
         }
 
     class Django:
