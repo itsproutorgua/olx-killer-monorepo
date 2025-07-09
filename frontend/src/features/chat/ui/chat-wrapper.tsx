426bb9c0c727bde2274cffaad0790c87ecb04f04
Demirov Vlad <demirov.vlad@gmail.com>
2025-07-09 17:59:12 +0300

    Similar products api integrated, bug fixes

--- frontend/src/features/chat/ui/chat-wrapper.tsx
+++ frontend/src/features/chat/ui/chat-wrapper.tsx
@@ -97,9 +97,9 @@ const ChatContent = ({
         <img
           src={chats_empty_image}
           alt='Chats Empty'
-          className='mb-[48px] max-h-[241px] max-w-[276px]'
+          className='mb-[22px] max-h-[186px] max-w-[213] xl:mb-[48px] xl:max-h-[241px] xl:max-w-[276px]'
         />
-        <p className='mb-4 text-xl font-medium'>
+        <p className='mb-4 max-w-[271px] text-xl font-medium xl:max-w-[512px]'>
           {t('messages.noConversations')}
         </p>
         <p className='max-w-[512px]'>{t('messages.startChatHint')}</p>
--- frontend/src/widgets/similar-products/similar-products.tsx
+++ frontend/src/widgets/similar-products/similar-products.tsx
@@ -2,25 +2,28 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { ScrollableProductList } from '@/widgets/scrollable-product-list/scrollable-product-list.tsx'
-import { useLatestProducts } from '@/entities/product/library/hooks/use-latest-products.tsx'
+import { useProducts } from '@/entities/product'
 import { ScrollableProductsSkeleton } from '@/shared/ui/skeletons'
 
 export interface SimilarProductProps {
   onProductClick: (slug: string) => void
+  path?: string
 }
 
 export const SimilarProducts: React.FC<SimilarProductProps> = ({
   onProductClick,
+  path,
 }) => {
   const { t } = useTranslation()
-  const { data, cursor } = useLatestProducts({
-    Skeleton: <ScrollableProductsSkeleton />,
-  })
+  const { data, cursor } = useProducts(
+    { path: path || '', limit: 10 },
+    { Skeleton: <ScrollableProductsSkeleton /> },
+  )
 
   return (
     <ScrollableProductList
       title={t('title.similarProducts')}
-      data={data}
+      data={data?.results}
       cursor={cursor}
       scrollStep={3} // Number of items to scroll
       className='absolute flex w-[107%] overflow-hidden'
